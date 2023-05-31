2fd68845b8005dbf9e0947d7d08f8dea28866491
Saulius Menkevicius <saulius@forallsecure.com>
2023-05-31 14:22:58 +0300

    test.c: add buffer overflow and setup an attempt at data execution

--- test.c
+++ test.c
@@ -6,12 +6,41 @@ void fn() {
 
 int main(int argc, char** argv)
 {
-    int c;
-    do {
-        c = getchar();
-    } while (c >= 0);
+    void (*pfn0)(void);
+    char buffer[10];
+    void (*pfn1)(void);
+    int buffer_i = 0;
+    void (*pfn2)(void);
 
-    fn();
+    pfn0 = fn;
+    pfn1 = fn;
+    pfn2 = fn;
+
+    pfn0();
+    pfn1();
+    pfn2();
+
+    while (1) {
+        int c = getchar();
+
+        if (c < 0) {
+            break;
+        }
+
+        buffer[buffer_i++] = c;
+    }
+
+    buffer[buffer_i] = 0; // add terminator
+
+    pfn0();
+    pfn1();
+    pfn2();
+
+    printf("buffer=%s\n", buffer);
+
+    pfn0();
+    pfn1();
+    pfn2();
 
     return 0;
 }
